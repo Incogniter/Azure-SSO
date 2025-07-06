305c46fb11d13239ef31ad534ff46acd1fadd274
abino.robilin <abino.robilin@mu-sigma.com>
2025-07-07 00:27:31 +0530

    Added CSRF Protection since cookies is being used

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.use(cookieParser());
+  app.use(cookieParser());  
+  app.use(csurf({ cookie: true }));
   app.enableCors({
-    origin: 'http://localhost:3000', 
-    credentials: true,              
+    origin: 'http://localhost:3000',
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 1433);
 }
