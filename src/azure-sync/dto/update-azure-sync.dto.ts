692d57ffd513f3b7598824f1b17bd8df845d9476
abino.robilin <abino.robilin@mu-sigma.com>
2025-07-06 23:30:20 +0530

    added code to fetch and store user role level details in db

--- src/azure-sync/dto/update-azure-sync.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAzureSyncDto } from './create-azure-sync.dto';
-
-export class UpdateAzureSyncDto extends PartialType(CreateAzureSyncDto) {}
